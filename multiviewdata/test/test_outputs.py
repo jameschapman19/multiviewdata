b847e2e011d344d103ec8b17a6fe45c6202b49a8
James Chapman <james.chapman.19@ucl.ac.uk>
2022-03-03 16:02:26 +0000

    Cool everything working!

--- multiviewdata/test/test_outputs.py
+++ multiviewdata/test/test_outputs.py
@@ -1,9 +1,26 @@
 import os
 
-from multiviewdata.torchdatasets import NoisyMNISTDataset
-
 
 def test_noisymnist():
+    from multiviewdata.torchdatasets import NoisyMNISTDataset
     a = NoisyMNISTDataset(os.getcwd(), download=True)[0]
+    assert "index" in a
+    assert "views" in a
+
+def test_tangledmnist():
+    from multiviewdata.torchdatasets import TangledMNISTDataset
+    a = TangledMNISTDataset(os.getcwd(), download=True)[0]
+    assert "index" in a
+    assert "views" in a
+
+def test_splitmnist():
+    from multiviewdata.torchdatasets import SplitMNISTDataset
+    a = SplitMNISTDataset(os.getcwd(), download=True)[0]
+    assert "index" in a
+    assert "views" in a
+
+def test_mfeat():
+    from multiviewdata.torchdatasets import MFeatDataset
+    a = MFeatDataset(os.getcwd(), download=True)[0]
     assert "index" in a
     assert "views" in a
