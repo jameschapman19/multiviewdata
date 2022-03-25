cc211c980c0a306a0555369d37ea8d6eff1fafba
James Chapman <james.chapman.19@ucl.ac.uk>
2022-03-25 12:37:06 +0000

    Cool everything working!

--- multiviewdata/test/test_outputs.py
+++ multiviewdata/test/test_outputs.py
@@ -2,25 +2,25 @@ import os
 
 
 def test_noisymnist():
-    from multiviewdata.torchdatasets import NoisyMNISTDataset
-    a = NoisyMNISTDataset(os.getcwd(), download=True)[0]
+    from multiviewdata.torchdatasets import NoisyMNIST
+    a = NoisyMNIST(os.getcwd(), download=True)[0]
     assert "index" in a
     assert "views" in a
 
 def test_tangledmnist():
-    from multiviewdata.torchdatasets import TangledMNISTDataset
-    a = TangledMNISTDataset(os.getcwd(), download=True)[0]
+    from multiviewdata.torchdatasets import TangledMNIST
+    a = TangledMNIST(os.getcwd(), download=True)[0]
     assert "index" in a
     assert "views" in a
 
 def test_splitmnist():
-    from multiviewdata.torchdatasets import SplitMNISTDataset
-    a = SplitMNISTDataset(os.getcwd(), download=True)[0]
+    from multiviewdata.torchdatasets import SplitMNIST
+    a = SplitMNIST(os.getcwd(), download=True)[0]
     assert "index" in a
     assert "views" in a
 
 def test_mfeat():
-    from multiviewdata.torchdatasets import MFeatDataset
-    a = MFeatDataset(os.getcwd(), download=True)[0]
+    from multiviewdata.torchdatasets import MFeat
+    a = MFeat(os.getcwd(), download=True)[0]
     assert "index" in a
     assert "views" in a
